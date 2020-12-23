e5ae593fc75c164b946ece3655799a104ee3aa6d
Rajas1312 <you@example.com>
2020-12-24 00:10:15 +0530

    Refactored:using constructor for mood analyser

--- main/java/com/dummytesting/MoodAnalyser.java
+++ main/java/com/dummytesting/MoodAnalyser.java
@@ -1,9 +1,13 @@
 package com.dummytesting;
 
 public class MoodAnalyser {
-    public static String analyseMood(String message){
 
-       if(message.contains("Sad")){
+    private String message;
+    public MoodAnalyser(String message){
+        this.message=message;
+    }
+    public String analyseMood(){
+       if(this.message.contains("Sad")){
            return "Sad";
        }
        else{
--- test/java/com/dummytesting/MoodAnalyserTest.java
+++ test/java/com/dummytesting/MoodAnalyserTest.java
@@ -6,15 +6,15 @@ import org.junit.Test;
 public class MoodAnalyserTest {
     @Test
     public void givenMood_MoodAnalyser_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser=new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This is a Sad message");
+        MoodAnalyser moodAnalyser=new MoodAnalyser("This is a Sad message");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("Sad",mood);
     }
 
     @Test
     public void givenMood_WhenAnyMood_ShouldReturnHappy() {
-        MoodAnalyser moodAnalyser=new MoodAnalyser();
-        String mood =moodAnalyser.analyseMood("This is Any mood");
+        MoodAnalyser moodAnalyser=new MoodAnalyser("This is Any mood");
+        String mood =moodAnalyser.analyseMood();
         Assert.assertEquals("Happy",mood);
     }
 }
